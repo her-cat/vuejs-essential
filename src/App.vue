224d4032a6547b53d6eecaf86de11ce0c5de5fad
hexianghui <hxhsoft@foxmail.com>
2019-02-27 19:40:49 +0800

    顶部导航

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Welcome to Vue</h1>
+  <div id="wrap">
+    <TheHeader/>
   </div>
 </template>
 
 <script>
+  import TheHeader from '@/components/layouts/TheHeader'
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      TheHeader
+    }
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 200px;
-  }
+<style lang="scss">
+  $container-large-desktop: 1200px;
+  $btn-primary-bg: #00b5ad;
+  $btn-primary-border: #00b5ad;
+  $label-primary-bg: #00b5ad;
+  $pagination-active-bg: #00b5ad;
+  $pagination-active-border: #00b5ad;
+  $pagination-color: #00b5ad;
+  $input-border-focus: #00b5ad;
+  $link-color: #12c4c5;
+  $link-hover-color: #22ddde;
+  $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+  $fa-font-path: "~font-awesome/fonts/";
+
+  @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+  @import "~font-awesome/scss/font-awesome";
+  @import "./styles/main";
+  @import "./styles/extra";
 </style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', {in: showCollapseNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index == activeNavIndex}">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TheHeader',
+    data() {
+      return {
+        logo: {
+          src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+          title: 'VuejsCaff'
+        },
+        navList: ['社区', '头条', '问答', '教程'],
+        activeNavIndex: 0,
+        showCollapseNav: false
+      }
+    },
+    beforeCreate() {
+      this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
+    },
+    methods: {
+      changeNavIndex(index) {
+        this.activeNavIndex = index
+      },
+      toggleNav() {
+        this.showCollapseNav = !this.showCollapseNav;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title { display: none; }
+  .navbar-default .navbar-nav > .active > a { background-color: rgba(0,0,0,.03); }
+</style>
