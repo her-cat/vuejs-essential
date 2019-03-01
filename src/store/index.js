e1ffa5dd9a61c2075cae82af9761f988e74e2421
her-cat <hxhsoft@foxmail.com>
2019-03-01 23:57:56 +0800

    格式化代码缩进

--- src/store/index.js
+++ src/store/index.js
@@ -6,41 +6,41 @@ import router from '../router'
 Vue.use(Vuex)
 
 const state = {
-    // 用户信息，初始值从本地 localStorage 获取
-    user: ls.getItem('user'),
-    // 添加 auth 来保存当前用户的登录状态
-    auth: ls.getItem('auth')
+// 用户信息，初始值从本地 localStorage 获取
+  user: ls.getItem('user'),
+  // 添加 auth 来保存当前用户的登录状态
+  auth: ls.getItem('auth')
 }
 
 const mutations = {
-    USER_UPDATE(state, user) {
-        // 改变 user 的值
-        state.user = user
-        // 将改变后的值存入 localStorage
-        ls.setItem('user', user)
-    },
-    // 添加 UPDATE_AUTH 来更改当前用户的登录状态
-    UPDATE_AUTH(state, auth) {
-      state.auth = auth;
-      ls.setItem('auth', auth);
-    }
+  USER_UPDATE(state, user) {
+    // 改变 user 的值
+    state.user = user
+    // 将改变后的值存入 localStorage
+    ls.setItem('user', user)
+  },
+  // 添加 UPDATE_AUTH 来更改当前用户的登录状态
+  UPDATE_AUTH(state, auth) {
+    state.auth = auth;
+    ls.setItem('auth', auth);
+  }
 }
 
 const actions = {
-    login({ commit }, user) {
-        // 登录时有传用户信息，就更新下用户信息
-        if (user) commit('UPDATE_USER', user)
-        // 更新当前用的登录状态为已登录
-        commit('UPDATE_AUTH', true)
-        // 跳转到首页
-        router.push('/')
-    }
+  login({ commit }, user) {
+    // 登录时有传用户信息，就更新下用户信息
+    if (user) commit('UPDATE_USER', user)
+    // 更新当前用的登录状态为已登录
+    commit('UPDATE_AUTH', true)
+    // 跳转到首页
+    router.push('/')
+  }
 }
 
 const store = new Vuex.Store({
-    state,
-    mutations,
-    actions
+  state,
+  mutations,
+  actions
 })
 
 export default store
