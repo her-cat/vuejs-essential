c1e56b03723fe91316e55cdffd16301a3b6a2fb5
her-cat <hxhsoft@foxmail.com>
2019-03-03 17:31:34 +0800

    注册页面增加 floating-box

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-4 col-md-offset-4 floating-box">
       <!-- 消息组件 -->
       <Message :show.sync="msgShow" :type="msgType" :msg="msg"/>
 
