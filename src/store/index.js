5a3ecb0722bed1cf606d0ece035aa89b2ad4392b
her-cat <hxhsoft@foxmail.com>
2019-03-06 20:17:55 +0800

    添加发布逻辑

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
+import * as moreActions from './actions'
 
 Vue.use(Vuex)
 
 const state = {
-// 用户信息，初始值从本地 localStorage 获取
+  // 用户信息，初始值从本地 localStorage 获取
   user: ls.getItem('user'),
   // 添加 auth 来保存当前用户的登录状态
-  auth: ls.getItem('auth')
+  auth: ls.getItem('auth'),
+  // 所有文章状态
+  articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -23,6 +26,11 @@ const mutations = {
   UPDATE_AUTH(state, auth) {
     state.auth = auth;
     ls.setItem('auth', auth);
+  },
+  // 更改所有文章的事件类型
+  UPDATE_ARTICLES(state, articles) {
+    state.articles = articles
+    ls.setItem('articles', articles)
   }
 }
 
@@ -46,7 +54,9 @@ const actions = {
     }
 
     commit('UPDATE_USER', user)
-  }
+  },
+  // 使用对象展开运算符混入 moreActions
+  ...moreActions
 }
 
 const store = new Vuex.Store({
