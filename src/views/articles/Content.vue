326c63d2f7aad0f41c18ccac499f98a043545d17
hexianghui <hxhsoft@foxmail.com>
2019-03-21 21:34:18 +0800

    添加点赞功能

--- src/views/articles/Content.vue
+++ src/views/articles/Content.vue
@@ -19,6 +19,25 @@
         </div>
       </div>
     </div>
+    <!-- 点赞 -->
+    <div class="votes-container panel panel-default padding-md">
+      <div class="panel-body vote-box text-center">
+        <div class="btn-group">
+          <a @click="like" href="javascript:;" class="vote btn btn-primary popover-with-html" :class="likeClass">
+            <i class="fa fa-thumbs-up"></i> {{ likeClass ? '已赞' : '点赞' }}
+          </a>
+        </div>
+        <div class="voted-users">
+          <div class="user-lists">
+        <span v-for="likeUser in likeUsers">
+          <!-- 点赞用户是当前用户时，加上类 animated 和 swing 以显示一个特别的动画  -->
+          <img :src="user && user.avatar" class="img-thumbnail avatar avatar-middle" :class="{ 'animated swing' : likeUser.uid === 1 }">
+        </span>
+          </div>
+          <div v-if="!likeUsers.length" class="vote-hint">成为第一个点赞的人吧 ?</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,6 +55,8 @@
         content: '',
         date: '',
         uid: 1,
+        likeUsers: [],
+        likeClass: '',
       }
     },
     computed: {
@@ -49,12 +70,14 @@
       const article = this.$store.getters.getArticleById(articleId)
 
       if (article) {
-        let {uid, title, content, date} = article
+        let { uid, title, content, date, likeUsers } = article
 
         this.uid = uid
         this.title = title
         this.content = SimpleMDE.prototype.markdown(emoji.emojify(content, name => name))
         this.date = date
+        this.likeUsers = likeUsers || []
+        this.likeClass = this.likeUsers.some(likeUser => likeUser.uid === 1) ? 'active' : ''
 
         this.$nextTick(() => {
           this.$el.querySelectorAll('pre code').forEach((el) => [
@@ -78,11 +101,45 @@
             this.$store.dispatch('post', { articleId: this.articleId })
           }
         })
+      },
+      like(e) {
+        // 未登录时，提示登录
+        if (!this.auth) {
+          this.$swal({
+            text: '需要登录以后才能执行此操作。',
+            confirmButtonText: '前往登录'
+          }).then((res) => {
+            if (res.value) {
+              this.$router.push('/auth/login')
+            }
+          })
+        } else {
+          const target = e.currentTarget
+          // 点赞按钮是否含有 active 类，我们用它来判断是否已赞
+          const active = target.classList.contains('active')
+          const articleId = this.articleId
+
+          if (active) {
+            // 清除已赞样式
+            this.likeClass = ''
+            // 分发 like 事件取消赞，更新实例的 likeUsers 为返回的值
+            this.$store.dispatch('like', { articleId }).then((likeUsers) => {
+              this.likeUsers = likeUsers
+            })
+          } else {
+            // 添加已赞样式
+            this.likeClass = 'active animated rubberBand'
+            // 分发 like 事件，传入 isAdd 参数点赞，更新实例的 likeUsers 为返回的值
+            this.$store.dispatch('like', { articleId, isAdd: true }).then((likeUsers) => {
+              this.likeUsers = likeUsers
+            })
+          }
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-
+  @import "animate.css/animate.min.css";
 </style>
